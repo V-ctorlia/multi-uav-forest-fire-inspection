53330b7e29da0f9320d4e9e968d07d996abebd34
V-ctorlia <148203580+V-ctorlia@users.noreply.github.com>
2026-09-06 22:22:57 +0800

    Delete DMOGWO directory

--- DMOGWO/EvaluatePopulation.m
+++
@@ -1,7 +0,0 @@
-function z = EvaluatePopulation(population, inspectionPoints, startPoint, numDrones)
-    % 评估种群的适应度，即目标函数的计算
-
-        [f1, f2] = CalculateObjectives(population, inspectionPoints, startPoint, numDrones);
-
-z = [f1 f2]';
-end
--- DMOGWO/hamming_distance.m
+++
@@ -1,8 +0,0 @@
-function hd = hamming_distance(X1, X2)
-    % 计算X1和X2之间的汉明距离
-    % 输入：X1, X2 - 两个位置向量（任务序列 + 分配断点）
-    % 输出：hd - 汉明距离
-    
-    hd = sum(X1 ~= X2);  % 计算不同元素的数量
-end
-
